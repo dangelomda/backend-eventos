8e268dae0ca631a21ade6dc718a6f7f116c76059
dangelomda <dangeloavila@terra.com.br>
2025-03-02 18:15:46 -0300

    Correção createdAt e updatedAt no Participante.js

---
+++ models/Participante.js
@@ -0,0 +1,34 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+const Participante = sequelize.define("Participante", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  nome: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  telefone: {
+    type: DataTypes.STRING,
+  },
+  createdAt: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW
+  },
+  updatedAt: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW
+  }
+}, {
+  timestamps: true  // Agora o Sequelize vai entender que essas colunas existem
+});
+
+module.exports = Participante;
